4d4bc6cbf123de0363862244f89993c5c8234bdc
Sungwon <63222221+mete0rfish@users.noreply.github.com>
2025-04-24 10:44:14 +0900

    [Silver III] Title: N과 M (3), Time: 344 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+
+using namespace std;
+
+int N, M;
+int output[9];
+bool vst[9];
+
+void func(int len, int idx) {
+    if(len == M) {
+        for(int i=0;i<M;i++) {
+            cout << output[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for(int i=1;i<=N;i++) {
+        //if(vst[i])  continue;
+
+        vst[i] = true;
+        output[len] = i;
+        func(len + 1, i + 1);
+        vst[i] = false;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N >> M;
+    func(0, 1);
+}
