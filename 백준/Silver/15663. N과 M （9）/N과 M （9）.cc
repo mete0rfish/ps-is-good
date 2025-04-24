49cafbd7cc25f5c4b747dd448a37105a89c0e257
Sungwon <63222221+mete0rfish@users.noreply.github.com>
2025-04-24 11:06:58 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
+
+using namespace std;
+
+int N, M;
+int input[9];
+int output[9];
+bool vst[9];
+bool used[10001];
+
+void func(int len) {
+    if(len == M) {
+        for(int i=0;i<M;i++) {
+            cout << output[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    int prev = 0;
+    for(int i=0;i<N;i++) {
+        if(vst[i])  continue;
+        if(prev == input[i])  continue;
+
+        vst[i] = true;
+        prev = input[i];
+        output[len] = input[i];
+        func(len + 1);
+        vst[i] = false;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N >> M;
+    for(int i=0;i<N;i++) {
+        cin >> input[i];
+    }
+
+    sort(input, input+N);
+    func(0);
+}
