fdd5c6b2352477d07b60dbfa3d580f299e08ed81
Sungwon <63222221+mete0rfish@users.noreply.github.com>
2025-04-24 10:28:30 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2160 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -30,11 +30,11 @@ vector<country> v;
 bool cmp(country a, country b) {
     if(a.gold == b.gold) {
         if(a.silver == b.silver) {
-            return a.bronze < b.bronze;
+            return a.bronze > b.bronze;
         }
-        return a.silver < b.silver;
+        return a.silver > b.silver;
     }
-    return a.gold < b.gold;
+    return a.gold > b.gold;
 }
 
 int main() {
@@ -51,13 +51,18 @@ int main() {
     sort(v.begin(), v.end(), cmp);
 
     int rank = 1;
+    int same = 0;
     for(int i=0;i<v.size();i++) {
         if(i > 0 && (v[i].gold != v[i-1].gold || v[i].silver != v[i-1].silver || v[i].bronze != v[i-1].bronze)) {
-            rank++;
+            rank+=same;
+            same = 0;
         }
 
         if(K == v[i].id) {
             cout << rank;
+            break;
         }
+
+        same++;
     }
 }
