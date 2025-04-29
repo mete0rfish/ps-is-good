9e465a9be204b9a24c199a1079c5ce8c116177b6
Sungwon <63222221+mete0rfish@users.noreply.github.com>
2025-04-29 16:23:34 +0900

    [Silver II] Title: 주식, Time: 612 ms, Memory: 10336 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.cc
+++ 백준/Silver/11501. 주식/주식.cc
@@ -1,42 +1,41 @@
 #include <iostream>
-#include <vector>
-#include <stack>
+#include <cstring>
 #include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <set>
 
 using namespace std;
 
-int arr[1000001];
+int T, N;
+int tmp;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
+    cin >> T;
+    while(T--) {
+        vector<int> v;
+        cin >> N;
+        for(int i=0;i<N;i++) {
+            cin >> tmp;
+            v.push_back(tmp);
+        }
 
-	int t, n;
-	cin >> t;
-	while (t--) {
-		cin >> n;
-		for (int i = 1; i <= n; i++) {
-			cin >> arr[i];
-		}
-
-
-		int large = -1;
-		long long sum = 0;
-		for (int cur = n; cur >= 1; cur--) {
-			if (large < arr[cur]) {
-				//cout << "changed: " << large << endl;
-				large = max(large, arr[cur]);
-			}
-				
-			else if (large >= arr[cur]) {
-				int tmp = large - arr[cur];
-				sum += tmp;
-			}
-		}
-
-		cout << sum << "\n";
-	}
-
+        int large = -1;
+        long sum = 0;
+        for(int i=N-1;i>=0;i--) {
+            if(large < v[i]) {
+                large = v[i];
+            }
+            else {
+                sum += (large - v[i]);
+            }
+        }
+        cout << sum << "\n";
+    }
 
 }
