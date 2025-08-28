2126bc02f3816e031a67484538ccd90b2630c79d
Sungwon <63222221+mete0rfish@users.noreply.github.com>
2025-08-28 15:01:19 +0900

    [Gold III] Title: 거울 설치, Time: 0 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.cc
+++ 백준/Gold/2151. 거울 설치/거울 설치.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,18 +9,19 @@ typedef struct {
     int cnt, x, y, dir;
 }info;
 
-int N;
-char arr[51][51];
-bool vst[5][51][51];
-vector<pair<int, int>> doors;
-int px[4] = {-1,0,1,0}; // 위, 오른쪽, 아래, 왼쪽
-int py[4] = {0,1,0,-1};
-
 struct cmp {
     bool operator()(const info& a, const info& b) {
         return a.cnt > b.cnt;
-    }
+    } 
 };
+
+int N;
+char arr[51][51];
+bool vst[5][51][51];
+int px[4] = {-1,0,1,0};
+int py[4] = {0,-1,0,1};
+priority_queue<info, vector<info>, cmp> q;
+vector<pair<int, int>> doors;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -36,52 +38,54 @@ int main() {
         }
     }
 
-    priority_queue<info, vector<info>, cmp> pq;
-    int x = doors[0].first;
-    int y = doors[0].second;
+    int dx = doors[0].first;
+    int dy = doors[0].second;
     for(int i=0;i<4;i++) {
-        pq.push({0, x, y, i});
-        // vst[i][x][y] = true; // TODO
+        q.push({0,dx,dy,i});
     }
-    
-    while(!pq.empty()) {
-        info cur = pq.top();
-        pq.pop();
 
-        if(cur.x == doors[1].first && cur.y == doors[1].second) {
-            cout << cur.cnt;
+
+    while(!q.empty()) {
+        int cnt = q.top().cnt;
+        int x = q.top().x;
+        int y = q.top().y;
+        int dir = q.top().dir;
+        q.pop();
+
+        if(x == doors[1].first && y == doors[1].second) {
+            cout << cnt;
             return 0;
         }
 
-        if(vst[cur.dir][cur.x][cur.y]) continue;
-        vst[cur.dir][cur.x][cur.y] = true;
+        if(vst[dir][x][y]) continue;
+        vst[dir][x][y] = true;
 
-        for(int i=0;i<N;i++) {
-            int nx = cur.x + px[cur.dir] * i;
-            int ny = cur.y + py[cur.dir] * i;
+        for(int i=0;i<N;i++){
+            int nx = x + px[dir]*i;
+            int ny = y + py[dir]*i;
 
-            if (nx < 0 || ny < 0 || nx >= N || ny >= N || arr[nx][ny] == '*') break;
-            
+            if(nx < 0 || ny < 0 || nx >= N || ny >= N) break;
+            if(arr[nx][ny] == '*') break;
 
-            if (arr[nx][ny] == '!') {
-                // 위, 아래일 경우, 왼쪽 또는 오른쪽
-                if(cur.dir==0 || cur.dir==2) {
-                    if (!vst[cur.dir][nx][ny]) {
-                        pq.push({cur.cnt+1, nx, ny, 1});
-                        pq.push({cur.cnt+1, nx, ny, 3});
+            if(arr[nx][ny] == '!') {
+                if(dir == 0 || dir == 2) {
+                    if(!vst[dir][nx][ny]) {
+                        q.push({cnt+1, nx, ny, 1});
+                        q.push({cnt+1, nx, ny, 3});
                     }
                 }
-                // 왼쪽, 오른쪽일 경우, 아래 또는 위
                 else {
-                    if (!vst[cur.dir][nx][ny]) {
-                        pq.push({cur.cnt+1, nx, ny, 0});
-                        pq.push({cur.cnt+1, nx, ny, 2});
+                    if(!vst[dir][nx][ny]) {
+                        q.push({cnt+1, nx, ny, 0});
+                        q.push({cnt+1, nx, ny, 2});
                     }
                 }
             }
-            if (arr[nx][ny] == '#') pq.push({cur.cnt, nx, ny, -1});
+            if(arr[nx][ny] == '#') {
+                q.push({cnt, nx, ny, 0});
+            }
         }
+        
     }
-    
     return 0;
 }
