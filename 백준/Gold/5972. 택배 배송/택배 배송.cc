963215910003ff715b015b803f8992234eb13647
Sungwon <63222221+mete0rfish@users.noreply.github.com>
2025-09-19 10:09:51 +0900

    [Gold V] Title: 택배 배송, Time: 24 ms, Memory: 5532 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.cc
+++ 백준/Gold/5972. 택배 배송/택배 배송.cc
@@ -1,52 +1,45 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
-const int INF = 1e9;
 int N, M;
-vector<pair<int, int>> edges[50001];
+int A, B, C;
 int dist[50001];
+vector<pair<int ,int>> edges[50001];
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     cin >> N >> M;
-    for (int i = 0; i < M; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        // 1. {목적지, 비용} 순서로 저장
-        edges[a].push_back({b, c});
-        edges[b].push_back({a, c});
+    for(int i=0;i<M;i++) {
+        cin >> A >> B >> C;
+        edges[A].push_back(make_pair(B, C));
+        edges[B].push_back(make_pair(A, C));
     }
 
-    fill(&dist[1], &dist[N + 1], INF);
-    // greater<> 사용 (더 간결함)
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    fill(&dist[0], &dist[50001], 1e9);
+    priority_queue<pair<int ,int>, vector<pair<int ,int>>, greater<pair<int,int>>> pq;
 
+    pq.push(make_pair(0, 1));
     dist[1] = 0;
-    // 우선순위 큐에는 {비용, 노드} 순서로 넣는 것이 정렬에 유리
-    pq.push({0, 1});
-
-    while (!pq.empty()) {
-        int cost = pq.top().first;
+    
+    while(!pq.empty()) {
+        int node_cost = pq.top().first;
         int node = pq.top().second;
         pq.pop();
 
-        if (dist[node] < cost) {
-            continue;
-        }
+        // 이거 주의
+        if(node_cost > dist[node]) continue;
 
-        for (auto& e : edges[node]) {
-            // 2. 저장된 순서에 맞게 값을 꺼냄
+        for(auto e : edges[node]) {
+            int edge_cost = e.second;
             int next_node = e.first;
-            int weight = e.second;
 
-            if (dist[next_node] > cost + weight) {
-                dist[next_node] = cost + weight;
+            if(dist[next_node] > dist[node] + edge_cost) {
+                dist[next_node] = dist[node] + edge_cost;
                 pq.push({dist[next_node], next_node});
             }
         }
