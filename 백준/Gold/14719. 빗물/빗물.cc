7379c17fd5b4c52ee183604a85af8dfec43da706
sungwon <63222221+mete0rfish@users.noreply.github.com>
2025-12-02 13:27:34 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.cc
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -1,37 +1,46 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <queue>
-#include <cstdlib>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
 
 using namespace std;
 
 int H, W;
-int arr[501];
-int left_max[501];
-int right_max[501];
+int res;
+// -> : 3 3 3 3 4 2 2 2 
+// <- : 3 3 3 3 4 4 4 4
+//    : 0 2 1 1 0 1 1 0
 
 int main() {
-	cin >> H >> W;
-	
-	for(int i=0;i<W;i++) cin >> arr[i];
+    
+    cin >> H >> W;
 
-	// -> : i 포함 최대 높이
+    vector<int> arr(W);
+    for(int i=0;i<W;i++) {
+        cin >> arr[i];
+    }
 
-	left_max[0] = arr[0];
-	for(int i=1;i<W;i++) {
-		left_max[i] = max(left_max[i-1], arr[i]);
-	}
-	right_max[W-1] = arr[W-1];
-	for(int i=W-2;i>=0;i--) {
-		right_max[i] = max(right_max[i+1], arr[i]);
-	}
+    
+    vector<int> left_max(W);
+    vector<int> right_max(W);
 
-	int res = 0;
-	for(int i=1;i<W-1;i++) {
-		int t = min(left_max[i], right_max[i]);
-		res += (t - arr[i]);
-	}
+    left_max[0] = arr[0];
+    for(int i=1;i<W;i++) {
+        left_max[i] = max(left_max[i-1], arr[i]);
+    }
 
-	cout << res;
+    right_max[W-1] = arr[W-1];
+    for(int i=W-2;i>=0;i--) {
+        right_max[i] = max(right_max[i+1], arr[i]);
+    }
+
+    for(int i=0;i<W;i++) {
+        res += min(left_max[i], right_max[i]) - arr[i];
+    }
+
+    cout << res;
+
+    return 0;
 }
